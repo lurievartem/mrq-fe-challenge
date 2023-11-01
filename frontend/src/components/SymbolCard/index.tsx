1d99f36c8cda3617d0be7530a16da912ed5b1807
Artem Luriev <lurievartem@gmail.com>
2023-11-01 23:08:07 +0200

    frontend technical task

--- frontend/src/components/SymbolCard/index.tsx
+++ frontend/src/components/SymbolCard/index.tsx
@@ -1,34 +1,53 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './symbolCard.css';
-import { ReactComponent as IndustryLogo } from '@/assets/industry.svg';
-import { ReactComponent as CompanyIcon } from '@/assets/company.svg';
-import { ReactComponent as MarketCapIcon } from '@/assets/market_cap.svg';
 import { useAppSelector } from '@/hooks/redux';
+import SymbolCardData from '@/components/SymbolCard/SymbolCardData';
+import SymbolCardPrice from '@/components/SymbolCard/SymbolCardPrice';
+import usePreviousValue from '@/hooks/usePreviousValue';
 
 type SymbolCardProps = {
   id: string;
   onClick: (symbolId: string) => void;
   price: number;
+  activeSymbol: string | null;
 };
 
-const SymbolCard = ({ id, onClick, price }: SymbolCardProps) => {
+const SymbolCard = ({ id, onClick, price, activeSymbol }: SymbolCardProps) => {
+  const [priceClass, setPriceClass] = useState('');
   const { trend, industry, companyName, marketCap } = useAppSelector(
     (state) => state.stocks.entities[id]
   );
+  const previousPrice = usePreviousValue(price);
+
   const handleOnClick = useCallback(() => {
     onClick(id);
   }, [id]);
 
+  useEffect(() => {
+    if (previousPrice && price !== previousPrice) {
+      const priceChangesClass = price > previousPrice ? 'symbolCard__priceUp' : 'symbolCard__priceDown';
+      const shakeChangesClass = price * 1.25 >= previousPrice || price * 0.75 <= previousPrice ? 'symbolCard__shake' : '';
+      setPriceClass(`${priceChangesClass} ${shakeChangesClass}`);
+    }
+
+    const interval = setInterval(() => {
+      setPriceClass('');
+    }, 1500);
+
+    return () => clearInterval(interval);
+  }, [price]);
+
+  const isActive = !activeSymbol ? '' : (activeSymbol === id ? 'symbolCard__active' : 'symbolCard__unactive');
+
   return (
-    <div onClick={handleOnClick} className="symbolCard">
-      <div>
-        {id} - {trend}
+    <div onClick={handleOnClick} className={`symbolCard ${isActive} ${priceClass}`}>
+      <div className={`symbolCardId ${trend ? 'symbolCardId__' + trend.toLowerCase() : ''}`}>
+        {id}
       </div>
-      <div>Price:</div>
-      <div>{price || 0} </div>
-      <CompanyIcon /> <div>{companyName}</div>
-      <IndustryLogo /> <div>{industry}</div>
-      <MarketCapIcon /> <div>{marketCap}</div>
+      <div className='symbolCardView'>
+        <SymbolCardPrice price={price} />
+        <SymbolCardData industry={industry} companyName={companyName} marketCap={marketCap} />
+      </div>
     </div>
   );
 };
--- frontend/src/components/SymbolsGrid/index.tsx
+++ frontend/src/components/SymbolsGrid/index.tsx
@@ -1,12 +1,14 @@
 import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '@/hooks/redux';
 import SymbolCard from '../SymbolCard';
+import Loading from '../Loading';
 import { fetchAllStocks, selectors } from '@/store/stocksSlice';
 type SymbolsGridProps = {
   onSymbolClick: (symbolId: string) => void;
+  activeSymbol: string | null;
 };
 
-const SymbolsGrid = ({ onSymbolClick }: SymbolsGridProps) => {
+const SymbolsGrid = ({ onSymbolClick, activeSymbol }: SymbolsGridProps) => {
   const stockSymbols = useAppSelector(selectors.selectStockIds);
   const prices = useAppSelector((state) => state.prices);
   const dispatch = useAppDispatch();
@@ -14,12 +16,16 @@ const SymbolsGrid = ({ onSymbolClick }: SymbolsGridProps) => {
     dispatch(fetchAllStocks());
   }, [dispatch]);
 
+  if (!stockSymbols?.length || !Object.keys(prices).length){
+    return <Loading/>;
+  }
+
   return (
-    <div>
-      {stockSymbols.map((id, i) => (
-        <SymbolCard price={prices[id]} onClick={onSymbolClick} key={i} id={id} />
+    <>
+      {stockSymbols.map((id) => (
+        <SymbolCard activeSymbol={activeSymbol} price={prices[id]} onClick={onSymbolClick} key={id} id={id} />
       ))}
-    </div>
+    </>
   );
 };
 
